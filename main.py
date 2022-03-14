cd5b7738c067d70b55bb67b4e0a6ac343ccb63cb
Artem <arteam_101@mail.ru>
2022-03-14 18:31:20 +0700

    Несколько изменений

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ url = 'https://coinmarketcap.com/'
 class Coin:
     full_name = ""
     short_name = ""
-    price = 0.0
-    p24h = ""
-    p7d = ""
-    market_cap = 0.0
-    volume24h = 0.0
-    circulating_supply = 0
+    price = ""
+    changes_per_today = ""
+    changes_per_week = ""
+    market_cap = ""
+    volume24h = ""
+    circulating_supply = ""
     
 driver = webdriver.Chrome('./chromedriver')
 driver.get(url)
-driver.execute_script("window.scrollTo(0,1250);")
+driver.execute_script("window.scrollTo(0,1500);")
 coins = []
 coins_rows = driver.find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')[0:25]
 for coin_row in coins_rows:
@@ -23,27 +23,27 @@ for coin_row in coins_rows:
     new_coin = Coin()
     new_coin.full_name = coin_row.find_elements_by_tag_name('p')[1].text
     new_coin.short_name = coin_row.find_elements_by_tag_name('p')[2].text
-    new_coin.price = float(coin_row.find_elements_by_tag_name('span')[2].text.replace('$', '').replace(',', ''))
+    new_coin.price = coin_row.find_elements_by_tag_name('span')[2].text
     
     if coin_row.find_elements_by_tag_name('span')[4].get_attribute('class') == 'icon-Caret-up':
-        new_coin.p24h = "+" + coin_row.find_elements_by_tag_name('span')[3].text
+        new_coin.changes_per_today = "+" + coin_row.find_elements_by_tag_name('span')[3].text
     else:
-        new_coin.p24h = "-" + coin_row.find_elements_by_tag_name('span')[3].text
+        new_coin.changes_per_today = "-" + coin_row.find_elements_by_tag_name('span')[3].text
         
     if coin_row.find_elements_by_tag_name('span')[6].get_attribute('class') == 'icon-Caret-up':
-        new_coin.p7d= "+" + coin_row.find_elements_by_tag_name('span')[5].text
+        new_coin.changes_per_week= "+" + coin_row.find_elements_by_tag_name('span')[5].text
     else:
-        new_coin.p7d = "-" + coin_row.find_elements_by_tag_name('span')[5].text
+        new_coin.changes_per_week = "-" + coin_row.find_elements_by_tag_name('span')[5].text
     
-    new_coin.market_cap = float(coin_row.find_elements_by_tag_name('span')[8].text.replace('$', '').replace(',', ''))
+    new_coin.market_cap = coin_row.find_elements_by_tag_name('span')[8].text
     
-    new_coin.volume24h = float(coin_row.find_elements_by_tag_name('p')[4].text.replace('$', '').replace(',', ''))
+    new_coin.volume24h = coin_row.find_elements_by_tag_name('p')[4].text
     
-    new_coin.circulating_supply = int(''.join(filter(str.isdigit, coin_row.find_elements_by_tag_name('p')[6].text.replace(',', ''))))
+    new_coin.circulating_supply = coin_row.find_elements_by_tag_name('p')[6].text
     
     coins.append(new_coin)
     
-    
+driver.__exit__()
 df = pandas.DataFrame([vars(coin) for coin in coins])
 df.index = df.index + 1
 df.to_csv('parsedData.csv', sep=';')
